9623c77464536b22ee7ba44234c67508fc8bdc8b
Alex <alexrybalka65@gmail.com>
2023-03-19 00:14:42 +0200

    Done  01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    bodyStyle: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', handledStartBtn);
+refs.stopBtn.addEventListener('click', handledStopBtn);
+let switcherColor = null;
+
+function handledStartBtn() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    switcherColor = setInterval(() => {
+        refs.bodyStyle.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function handledStopBtn() {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+    clearInterval(switcherColor);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
